e3d885fc95a8608a4e9d08b6c23d229af9fcc9ae
chani6887 <chsh6887@gmail.com>
2025-12-04 12:06:46 +0200

    feat(frontend): add GarageService to call backend API

---
+++ frontend/src/app/services/garage.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Garage } from '../models/garage';
+
+@Injectable({ providedIn: 'root' })
+export class GarageService {
+  private base = 'https://localhost:5001/api/garages';
+
+  constructor(private http: HttpClient) {}
+
+  getFromGov(limit = 5): Observable<Garage[]> {
+    return this.http.get<Garage[]>(`${this.base}/fromgov?limit=${limit}`);
+  }
+
+  getSaved(): Observable<Garage[]> {
+    return this.http.get<Garage[]>(this.base);
+  }
+
+  addMany(garages: Garage[]): Observable<Garage[]> {
+    return this.http.post<Garage[]>(`${this.base}/add`, garages);
+  }
+}
